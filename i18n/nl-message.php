683c79d16cef40bae4b280037fbc833b041f340a
NicNLD <nic-nld@live.nl>
2012-11-22 16:34:16 +0100

    Adding dutch translation

---
+++ i18n/nl-message.php
@@ -0,0 +1,58 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Catalog
+ * 
+ * The catalog plugin adds a catalog or webshop to Wolf CMS.
+ * 
+ * @package     Plugins
+ * @subpackage  catalog
+ * 
+ * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @copyright   Nic Wortel, 2012
+ * @version     0.0.1
+ */
+
+return array(
+    'Add attribute' => 'Attribuut toevoegen',
+    'Add brand' => 'Merk toevoegen',
+    'Add category' => 'Categorie toevoegen',
+    'Add product' => 'Product toevoegen',
+    'Attributes' => 'Attributen',
+    'Brand' => 'Merk',
+    'Brands' => 'Merken',
+    'Catalog' => 'Catalogus',
+    'Categories' => 'Categorieën',
+    'Category' => 'Categorie',
+    'Comma' => 'Komma',
+    'Decimal seperator' => 'Decimaal scheidingsteken',
+    'Default unit' => 'Standaard eenheid',
+    'Description' => 'Beschrijving',
+    'Documentation' => 'Documentatie',
+    'Edit attribute' => 'Attribuut bewerken',
+    'Edit brand' => 'Merk bewerken',
+    'Edit category' => 'Categorie bewerken',
+    'Edit product' => 'Product bewerken',
+    'Export' => 'Exporteren',
+    'Inherited attributes' => 'Overgenomen attributen',
+    'N/A' => 'n.v.t.',
+    'Name' => 'Naam',
+    'Point' => 'Punt',
+    'Price' => 'Prijs',
+    'Products' => 'Producten',
+    'Search' => 'Zoeken',
+    'Save Settings' => 'Instellingen opslaan',
+    'Settings' => 'Instellingen',
+    'Stock' => 'Voorraad',
+    'The catalog plugin adds a catalog or webshop to Wolf CMS.' => 'De catalog plugin voegt een catalogus of webshop toe aan Wolf CMS.',
+    'Title' => 'Titel',
+    'URL' => 'URL',
+    'VAT' => 'BTW',
+    'Variant' => 'Variant',
+    'Variants' => 'Varianten',
+    'View product' => 'Product bekijken',
+    'Website' => 'Website',
+    'Website URL' => 'URL Website',
+    'Weight' => 'Gewicht'
+);
