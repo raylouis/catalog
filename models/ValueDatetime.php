d2842f18070418d1b36101e09f4fe96c3f18b794
NicNLD <nic-nld@live.nl>
2012-11-27 23:47:08 +0100

    Added new value models

---
+++ models/ValueDatetime.php
@@ -0,0 +1,22 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Catalog
+ * 
+ * The catalog plugin adds a catalog or webshop to Wolf CMS.
+ * 
+ * @package     Plugins
+ * @subpackage  catalog
+ * 
+ * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @copyright   Nic Wortel, 2012
+ * @version     0.0.1
+ */
+
+use_helper('ActiveRecord');
+
+class ValueDatetime extends Value {
+    const TABLE_NAME = 'catalog_value_datetime';
+    
+}
