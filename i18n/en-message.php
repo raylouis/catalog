0deddf7fddc552e0a631813ca064af0939413bcb
NicNLD <nic-nld@live.nl>
2013-09-10 16:13:07 +0200

    Added en-message.php

---
+++ i18n/en-message.php
@@ -0,0 +1,91 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Catalog
+ * 
+ * The catalog plugin adds a catalog or webshop to Wolf CMS.
+ * 
+ * @package     Plugins
+ * @subpackage  catalog
+ * 
+ * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @copyright   Nic Wortel, 2012
+ * @version     0.1.0
+ */
+
+return array(
+    'Abbreviation' => 'Abbreviation',
+    'Add a new variant' => 'Add a new variant',
+    'Add attribute' => 'Add attribute',
+    'Add brand' => 'Add brand',
+    'Add category' => 'Add category',
+    'Add product' => 'Add product',
+    'Add unit' => 'Add unit',
+    'Add VAT rate' => 'Add VAT rate',
+    'Are you sure you wish to delete :name?' => 'Are you sure you wish to delete :name?',
+    'Attributes' => 'Attributes',
+    'Brand' => 'Brand',
+    'Brands' => 'Brands',
+    'Catalog' => 'Catalog',
+    'Categories' => 'Categories',
+    'Category' => 'Category',
+    'Choose one' => 'Choose one',
+    'Color' => 'Color',
+    'Comma' => 'Comma',
+    'Comparison' => 'Comparison',
+    'Decimal seperator' => 'Decimal seperator',
+    'Default unit' => 'Default unit',
+    'Description' => 'Description',
+    'Distance' => 'Distance',
+    'Documentation' => 'Documentation',
+    'Duration' => 'Duration',
+    'Edit attribute' => 'Edit attribute',
+    'Edit brand' => 'Edit brand',
+    'Edit category' => 'Edit category',
+    'Edit product' => 'Edit product',
+    'Edit unit' => 'Edit unit',
+    'Edit VAT rate' => 'Edit VAT rate',
+    'Export' => 'Export',
+    'General product info' => 'General product info',
+    'General settings' => 'General settings',
+    'ID' => 'ID',
+    'Inherited attributes' => 'Inherited attributes',
+    'Logo' => 'Logo',
+    'Manage units' => 'Manage units',
+    'N/A' => 'N/A',
+    'Name' => 'Name',
+    'None' => 'None',
+    'Number' => 'Number',
+    'Percentage' => 'Percentage',
+    'Point' => 'Point',
+    'Price' => 'Price',
+    'Products' => 'Products',
+    'Search' => 'Search',
+    'Save Settings' => 'Save Settings',
+    'Settings' => 'Settings',
+    'SKU' => 'SKU',
+    'Stock' => 'Stock',
+    'Subcategories' => 'Subcategories',
+    'Surface' => 'Surface',
+    'System' => 'System',
+    'Temperature' => 'Temperature',
+    'Text' => 'Text',
+    'The catalog plugin adds a catalog or webshop to Wolf CMS.' => 'The catalog plugin adds a catalog or webshop to Wolf CMS.',
+    'Title' => 'Title',
+    'Type' => 'Type',
+    'Unit' => 'Unit',
+    'Units' => 'Units',
+    'URL' => 'URL',
+    'VAT' => 'VAT',
+    'VAT rate' => 'VAT rate',
+    'VAT rates' => 'VAT rates',
+    'Variant' => 'Variant',
+    'Variants' => 'Variants',
+    'View product' => 'View product',
+    'Volume' => 'Volume',
+    'Website' => 'Website',
+    'Website URL' => 'Website URL',
+    'Weight' => 'Weight',
+    'Yes/No' => 'Yes/No'
+);
